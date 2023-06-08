1482fa635d5115945b062c0973d8815aeba0ae9f
lianaa98 <liana.kan98@gmail.com>
2023-06-08 15:46:28 -0700

    its recording meeee

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,13 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import React, { useState } from "react";
 import { themeColors } from "../theme";
+import Voice from "@react-native-voice/voice";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text>First Aide</Text>
+      <Button title="PRESS ME" onPress={() => Voice.start("en-US")} />
     </View>
   );
 }
