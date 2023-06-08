42e68f9e34e8dc011e77ed3e336d8421236decd8
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-07 21:49:47 -0700

    Initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { themeColors } from "../theme";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>First Aide</Text>
+    </View>
+  );
+}
---
+++ theme/index.js
@@ -0,0 +1,5 @@
+export const themeColors = {
+  primary: "#000",
+  secondary: "#000",
+  light: "#000",
+};
