988fa23ca3a997cf89ce1af49b4e351f2a257c17
lianaa98 <liana.kan98@gmail.com>
2023-06-08 16:26:30 -0700

    voice recognition is working

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, Button } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { themeColors } from "../theme";
 import Voice from "@react-native-voice/voice";
 
@@ -7,15 +7,62 @@ export default function HomeScreen() {
   const [voiceResult, setVoiceResult] = useState("");
   const [isRecording, setIsRecording] = useState(false);
 
-  recordHandler = () => {
-    Voice.start("en-US");
-    console.log("Recording...");
-    setIsRecording(true);
+  const onSpeechStart = (e) => {
+    console.log("onSpeechStart: ", e);
   };
 
+  const onSpeechEnd = (e) => {
+    console.log("onSpeechEnd: ", e);
+  };
+
+  const onSpeechError = (e) => {
+    console.log("onSpeechError: ", e);
+  };
+
+  const onSpeechResults = (e) => {
+    console.log("onSpeechResults: ", e);
+    const result = e.value[0];
+    setVoiceResult(result);
+  };
+
+  const clear = () => {
+    setVoiceResult("");
+  };
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const recordHandler = async () => {
+    console.log("Recording...");
+    setIsRecording(true);
+    try {
+      await Voice.start("en-US");
+    } catch (e) {
+      console.error("error:", e);
+    }
+  };
+
+  const stopRecordingHandler = async () => {
+    try {
+      await Voice.stop();
+      setIsRecording(false);
+    } catch (e) {
+      console.error("error:", e);
+    }
+  };
+
+  // when the user stops recording, the onSpeechResults callback is called
   Voice.onSpeechResults = (e) => {
-    console.log(e);
-    setVoiceResult(e.values[0]);
+    console.log("Speech Results", e.value);
+    setVoiceResult(e.value[0]);
+    setIsRecording(false);
   };
 
   return (
@@ -23,15 +70,9 @@ export default function HomeScreen() {
       <Text>First Aide</Text>
       <Button title="START RECORDING" onPress={recordHandler} />
       {isRecording && <Text>Recording...</Text>}
-
-      <Button
-        title="STOP RECORDING"
-        onPress={() => {
-          Voice.stop();
-          console.log("Stopped Recording");
-          setIsRecording(false);
-        }}
-      />
+      {voiceResult !== "" && <Text>{voiceResult}</Text>}
+      <Button title="STOP RECORDING" onPress={stopRecordingHandler} />
+      <Button title="CLEAR" onPress={clear} />
     </View>
   );
 }
