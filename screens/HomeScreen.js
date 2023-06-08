706d070adb01a5b0aca299a490244c84d0be238f
lianaa98 <liana.kan98@gmail.com>
2023-06-08 16:03:00 -0700

    can record and stop record

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,10 +4,34 @@ import { themeColors } from "../theme";
 import Voice from "@react-native-voice/voice";
 
 export default function HomeScreen() {
+  const [voiceResult, setVoiceResult] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+
+  recordHandler = () => {
+    Voice.start("en-US");
+    console.log("Recording...");
+    setIsRecording(true);
+  };
+
+  Voice.onSpeechResults = (e) => {
+    console.log(e);
+    setVoiceResult(e.values[0]);
+  };
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>First Aide</Text>
-      <Button title="PRESS ME" onPress={() => Voice.start("en-US")} />
+      <Button title="START RECORDING" onPress={recordHandler} />
+      {isRecording && <Text>Recording...</Text>}
+
+      <Button
+        title="STOP RECORDING"
+        onPress={() => {
+          Voice.stop();
+          console.log("Stopped Recording");
+          setIsRecording(false);
+        }}
+      />
     </View>
   );
 }
