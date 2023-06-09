aa3e7d8235a426588513c27c7face4f83bec13d4
lianaa98 <liana.kan98@gmail.com>
2023-06-09 11:40:00 -0700

    add login & register buttons

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,15 +1,22 @@
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button, Pressable } from "react-native";
 import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function Landing() {
+  const [loginPress, setLoginPress] = useState(false);
+  const [registerPress, setRegisterPress] = useState(false);
   return (
     <View style={styles.container}>
       <LinearGradient
         colors={["#FE0944", "#FEAE96"]}
         style={styles.linearGradient}
       >
-        <Text>LANDING SCREEN</Text>
+        <Pressable style={styles.button} onPress={() => alert("Pressed!")}>
+          <Text style={styles.text}>Login</Text>
+        </Pressable>
+        <Pressable style={styles.button} onPress={() => alert("Pressed!")}>
+          <Text style={styles.text}>Register</Text>
+        </Pressable>
       </LinearGradient>
     </View>
   );
@@ -24,5 +31,22 @@ const styles = StyleSheet.create({
     height: "100%",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 8,
+    paddingVertical: 12,
+    width: 200,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
   },
 });
