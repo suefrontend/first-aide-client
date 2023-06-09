bac897a8477a6f640fc56de69d8f68b8556285bb
lianaa98 <liana.kan98@gmail.com>
2023-06-09 10:56:53 -0700

    try gradient background

--- screens/components/Recorder.js
+++ screens/components/Recorder.js
@@ -1,5 +1,6 @@
-import { View, Text, Button } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 import Voice from "@react-native-voice/voice";
 
 export default function Recorder() {
@@ -58,13 +59,33 @@ export default function Recorder() {
   };
 
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>First Aide</Text>
-      <Button title="START RECORDING" onPress={recordHandler} />
-      {isRecording && <Text>Recording...</Text>}
-      {voiceResult !== "" && <Text>{voiceResult}</Text>}
-      <Button title="STOP RECORDING" onPress={stopRecordingHandler} />
-      <Button title="CLEAR" onPress={clear} />
+    <View
+      style={styles.container}
+      className="flex-1 items-center justify-center"
+    >
+      <LinearGradient
+        colors={["#FE0944", "#FEAE96"]}
+        style={styles.linearGradient}
+      >
+        <Text>Tell me your symptoms</Text>
+        <Button title="START RECORDING" onPress={recordHandler} />
+        {isRecording && <Text>Recording...</Text>}
+        {voiceResult !== "" && <Text>{voiceResult}</Text>}
+        <Button title="STOP RECORDING" onPress={stopRecordingHandler} />
+        <Button title="CLEAR" onPress={clear} />
+      </LinearGradient>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  linearGradient: {
+    width: "100%",
+    height: "100%",
+  },
+});
