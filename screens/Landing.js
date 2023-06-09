a0bc319d1c9adfffef8814b16704d69fa6bb4e44
lianaa98 <liana.kan98@gmail.com>
2023-06-09 12:36:07 -0700

    create forms and quick start button

--- screens/Landing.js
+++ screens/Landing.js
@@ -1,24 +1,115 @@
-import { StyleSheet, View, Text, Button, Pressable } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Pressable,
+  TextInput,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import HomeScreen from "./HomeScreen";
 
 export default function Landing() {
   const [loginPress, setLoginPress] = useState(false);
   const [registerPress, setRegisterPress] = useState(false);
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    city: "",
+  });
+
+  const clearUser = () => {
+    setUser({
+      name: "",
+      email: "",
+      city: "",
+    });
+  };
+
+  const [userAuthenticated, setUserAuthenticated] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        colors={["#FE0944", "#FEAE96"]}
-        style={styles.linearGradient}
-      >
-        <Pressable style={styles.button} onPress={() => alert("Pressed!")}>
-          <Text style={styles.text}>Login</Text>
-        </Pressable>
-        <Pressable style={styles.button} onPress={() => alert("Pressed!")}>
-          <Text style={styles.text}>Register</Text>
-        </Pressable>
-      </LinearGradient>
-    </View>
+    <>
+      {userAuthenticated == false && (
+        <View style={styles.container}>
+          <LinearGradient
+            colors={["#FE0944", "#FEAE96"]}
+            style={styles.linearGradient}
+          >
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Click me to quick start"
+                onPress={() => setUserAuthenticated(true)}
+              ></Button>
+              <Pressable
+                style={styles.button}
+                onPress={() => {
+                  setLoginPress(!loginPress);
+                  clearUser();
+                }}
+              >
+                <Text style={styles.text}>Login</Text>
+              </Pressable>
+              <Pressable
+                style={styles.button}
+                onPress={() => {
+                  setRegisterPress(!registerPress);
+                  clearUser();
+                }}
+              >
+                <Text style={styles.text}>Register</Text>
+              </Pressable>
+            </View>
+            {loginPress && (
+              <View style={styles.formContainer}>
+                <TextInput
+                  style={styles.text}
+                  placeholder="enter your email"
+                  placeholderTextColor="white"
+                  onChangeText={(text) => setUser({ ...user, email: text })}
+                />
+                <Pressable
+                  style={styles.button}
+                  onPress={() => console.log("Login:", user)}
+                >
+                  <Text style={styles.text}>Login</Text>
+                </Pressable>
+              </View>
+            )}
+            {registerPress && (
+              <View style={styles.formContainer}>
+                <TextInput
+                  style={styles.text}
+                  placeholder="enter your name"
+                  placeholderTextColor="white"
+                  onChangeText={(text) => setUser({ ...user, name: text })}
+                />
+                <TextInput
+                  style={styles.text}
+                  placeholder="enter your email"
+                  placeholderTextColor="white"
+                  onChangeText={(text) => setUser({ ...user, email: text })}
+                />
+                <TextInput
+                  style={styles.text}
+                  placeholder="enter your city"
+                  placeholderTextColor="white"
+                  onChangeText={(text) => setUser({ ...user, city: text })}
+                />
+                <Pressable
+                  style={styles.button}
+                  onPress={() => console.log("Register:", user)}
+                >
+                  <Text style={styles.text}>Register</Text>
+                </Pressable>
+              </View>
+            )}
+          </LinearGradient>
+        </View>
+      )}
+      {userAuthenticated && <HomeScreen />}
+    </>
   );
 }
 
@@ -48,5 +139,22 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     letterSpacing: 0.25,
     color: "white",
+  },
+  formText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  formContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
   },
 });
