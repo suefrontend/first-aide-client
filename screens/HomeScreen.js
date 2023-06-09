4a115674d45561308eff15a5d2ae8226f423b47e
lianaa98 <liana.kan98@gmail.com>
2023-06-08 20:31:11 -0700

    implement navigation tabs

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,70 +2,23 @@ import { View, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 import { themeColors } from "../theme";
 import Voice from "@react-native-voice/voice";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Recorder from "./components/Recorder";
+import MedicalInfo from "./components/MedicalInfo";
+import HospitalInfo from "./components/HospitalInfo";
+
+const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
-  const [voiceResult, setVoiceResult] = useState("");
-  const [isRecording, setIsRecording] = useState(false);
-
-  const onSpeechStart = (e) => {
-    console.log("onSpeechStart: ", e);
-  };
-
-  const onSpeechEnd = (e) => {
-    console.log("onSpeechEnd: ", e);
-  };
-
-  const onSpeechError = (e) => {
-    console.log("onSpeechError: ", e);
-  };
-
-  const onSpeechResults = (e) => {
-    console.log("onSpeechResults: ", e);
-    const result = e.value[0];
-    setVoiceResult(result);
-  };
-
-  const clear = () => {
-    setVoiceResult("");
-  };
-
-  useEffect(() => {
-    Voice.onSpeechStart = onSpeechStart;
-    Voice.onSpeechEnd = onSpeechEnd;
-    Voice.onSpeechError = onSpeechError;
-    Voice.onSpeechResults = onSpeechResults;
-    return () => {
-      Voice.destroy().then(Voice.removeAllListeners);
-    };
-  }, []);
-
-  const recordHandler = async () => {
-    console.log("Recording...");
-    setIsRecording(true);
-    try {
-      await Voice.start("en-US");
-    } catch (e) {
-      console.error("error:", e);
-    }
-  };
-
-  const stopRecordingHandler = async () => {
-    try {
-      await Voice.stop();
-      setIsRecording(false);
-    } catch (e) {
-      console.error("error:", e);
-    }
-  };
-
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>First Aide</Text>
-      <Button title="START RECORDING" onPress={recordHandler} />
-      {isRecording && <Text>Recording...</Text>}
-      {voiceResult !== "" && <Text>{voiceResult}</Text>}
-      <Button title="STOP RECORDING" onPress={stopRecordingHandler} />
-      <Button title="CLEAR" onPress={clear} />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Recorder" component={Recorder} />
+        <Tab.Screen name="Medical Info" component={MedicalInfo} />
+        <Tab.Screen name="Hospital Info" component={HospitalInfo} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ screens/components/Recorder.js
@@ -0,0 +1,70 @@
+import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import Voice from "@react-native-voice/voice";
+
+export default function Recorder() {
+  const [voiceResult, setVoiceResult] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+
+  const onSpeechStart = (e) => {
+    console.log("onSpeechStart: ", e);
+  };
+
+  const onSpeechEnd = (e) => {
+    console.log("onSpeechEnd: ", e);
+  };
+
+  const onSpeechError = (e) => {
+    console.log("onSpeechError: ", e);
+  };
+
+  const onSpeechResults = (e) => {
+    console.log("onSpeechResults: ", e);
+    const result = e.value[0];
+    setVoiceResult(result);
+  };
+
+  const clear = () => {
+    setVoiceResult("");
+  };
+
+  useEffect(() => {
+    Voice.onSpeechStart = onSpeechStart;
+    Voice.onSpeechEnd = onSpeechEnd;
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    };
+  }, []);
+
+  const recordHandler = async () => {
+    console.log("Recording...");
+    setIsRecording(true);
+    try {
+      await Voice.start("en-US");
+    } catch (e) {
+      console.error("error:", e);
+    }
+  };
+
+  const stopRecordingHandler = async () => {
+    try {
+      await Voice.stop();
+      setIsRecording(false);
+    } catch (e) {
+      console.error("error:", e);
+    }
+  };
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>First Aide</Text>
+      <Button title="START RECORDING" onPress={recordHandler} />
+      {isRecording && <Text>Recording...</Text>}
+      {voiceResult !== "" && <Text>{voiceResult}</Text>}
+      <Button title="STOP RECORDING" onPress={stopRecordingHandler} />
+      <Button title="CLEAR" onPress={clear} />
+    </View>
+  );
+}
