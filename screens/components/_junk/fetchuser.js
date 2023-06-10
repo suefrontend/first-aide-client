18e7b88d677140ac2b5fd3f733e3e2e0d30464b0
lianaa98 <liana.kan98@gmail.com>
2023-06-09 22:32:05 -0700

    add authenticated calls

--- screens/components/_junk/fetchuser.js
+++ screens/components/_junk/fetchuser.js
@@ -1,8 +1,10 @@
+import { authGet } from "../../helpers/authenticatedCalls";
+
 <Button
   title="click me to fetch user data"
   onPress={async () => {
     try {
-      const response = await axios.get("http://localhost:8000/users/");
+      const response = await authGet("http://localhost:8000/users/");
       console.log(response.data);
     } catch (error) {
       console.log(error);
