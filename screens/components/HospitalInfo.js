6d802d177077035534b491ecbe55b46f4a14bc58
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-11 13:20:01 -0700

    Add static data for hospital list

--- screens/components/HospitalInfo.js
+++ screens/components/HospitalInfo.js
@@ -2,6 +2,25 @@ import { View, Text, Button } from "react-native";
 import React, { useEffect, useState } from "react";
 
 export default function HospitalInfo() {
+  const hospitals = [
+    {
+      city_id: 1,
+      name: "Vancouver General Hospital",
+      address: "899 West 12th Avenue",
+      phone: "(604)-875-4111",
+      hours: "24 hours a day, 7 days a week",
+      er_exists: true,
+    },
+    {
+      city_id: 1,
+      name: "Vancouver General Hospital",
+      address: "899 West 12th Avenue",
+      phone: "(604)-875-4111",
+      hours: "24 hours a day, 7 days a week",
+      er_exists: true,
+    },
+  ];
+
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Hospital Information Screen</Text>
