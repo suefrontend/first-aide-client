7d1774479f512af455d2e7c45486ba934018a90c
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-11 13:49:20 -0700

    Add gradient background to Bookmarks page

---
+++ screens/components/Bookmarks.js
@@ -0,0 +1,21 @@
+import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function Bookmarks() {
+  return (
+    <View>
+      <LinearGradient
+        colors={["#FE0944", "#FEAE96"]}
+        style={styles.linearGradient}
+      ></LinearGradient>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    width: "100%",
+    height: "100%",
+  },
+});
