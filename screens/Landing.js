ddb00097821cc62a72d3e6ad1c771a806dac80d2
lianaa98 <liana.kan98@gmail.com>
2023-06-11 13:06:34 -0700

    toggle modal

--- screens/Landing.js
+++ screens/Landing.js
@@ -13,12 +13,13 @@ import { LinearGradient } from "expo-linear-gradient";
 import HomeScreen from "./HomeScreen";
 import Login from "./components/landing/Login";
 import Register from "./components/landing/Register";
+import Loader from "./components/loading/Loader";
 import { storeToken, getToken, removeToken } from "./helpers/tokenStorage";
 
 export default function Landing() {
   const [loginPress, setLoginPress] = useState(true);
   const [registerPress, setRegisterPress] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [userAuthenticated, setUserAuthenticated] = useState(false);
 
   useEffect(() => {
@@ -123,6 +124,10 @@ export default function Landing() {
     });
   };
 
+  toggleLoading = () => {
+    setIsLoading(!isLoading);
+  };
+
   return (
     <>
       {userAuthenticated == false && (
@@ -147,6 +152,7 @@ export default function Landing() {
                   <Pressable style={styles.button} onPress={switchHandler}>
                     <Text style={styles.text}>Create Account</Text>
                   </Pressable>
+                  {isLoading && <Loader toggleLoading={toggleLoading} />}
                 </>
               )}
               {registerPress && (
@@ -158,6 +164,10 @@ export default function Landing() {
                   cityTypeHandler={cityTypeHandler}
                 />
               )}
+              <Button
+                title="Click me to toggle loading screen"
+                onPress={toggleLoading}
+              />
             </View>
           </LinearGradient>
         </View>
--- screens/components/loading/Loader.js
+++ screens/components/loading/Loader.js
@@ -1,10 +1,24 @@
-import { View, Text, Button } from "react-native";
+import { View, Modal, Text, Button, StyleSheet } from "react-native";
 import React, { useEffect, useState } from "react";
 
-export default function Loader() {
+export default function Loader(props) {
+  const { toggleLoading } = props;
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>LOADING SCREEN</Text>
-    </View>
+    <Modal style={styles.centeredModal}>
+      <View style={styles.centeredModal}>
+        <Text>LOADING SCREEN</Text>
+        <Button title="Close" onPress={() => toggleLoading()} />
+      </View>
+    </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  centeredModal: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+});
