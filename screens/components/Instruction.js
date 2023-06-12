913c100f41bf41147895b817e70fac9fe2e49435
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-12 09:55:08 -0700

    Enable scrolling

--- screens/components/Instruction.js
+++ screens/components/Instruction.js
@@ -1,6 +1,14 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  TouchableHighlight,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function Instruction(props) {
   const { instructionKey, instructionDetail } = props;
@@ -10,41 +18,49 @@ export default function Instruction(props) {
         colors={["#FE0944", "#FEAE96"]}
         style={styles.linearGradient}
       >
-        <View>
-          <View style={styles.wrapper}>
-            <Text
-              className="py-6 text-2xl font-bold text-white"
-              style={styles.headings}
-            >
-              Emergency Contact
-            </Text>
-          </View>
+        <View className="pt-20">
           <View style={styles.wrapper}>
             {/* <Text>{instructionKey}</Text>
           <Text>{instructionDetail}</Text> */}
-            <View className="flex-row py-10 justify-between">
-              <Text className="text-3xl" style={styles.headings}>
+            <View className="flex-row py-10 justify-between items-center">
+              <Text
+                className="text-4xl text-white font-bold"
+                style={styles.headings}
+              >
                 Abrasions
               </Text>
-              <Button title="bookmark" />
+              <TouchableHighlight onPress={() => {}}>
+                <View>
+                  <Icon
+                    name="bookmark"
+                    size={20}
+                    color="#fff"
+                    style={styles.headings}
+                  />
+                </View>
+              </TouchableHighlight>
             </View>
-            <Text
-              style={[styles.headings, styles.text]}
-              className="text-white text-lg"
-            >
-              Begin with washed hands. Gently clean the area with cool to
-              lukewarm water and mild soap. Remove dirt or other particles from
-              the wound using sterilized tweezers. For a mild scrape that’s not
-              bleeding, leave the wound uncovered. If the wound is bleeding, use
-              a clean cloth or bandage and apply gentle pressure to the area to
-              stop any bleeding. Cover a wound that bled with a thin layer of
-              topical antibiotic ointment, like Bacitracin, or a sterile
-              moisture barrier ointment, like Aquaphor. Cover it with a clean
-              bandage or gauze. Gently clean the wound and change the ointment
-              and bandage once per day. Watch the area for signs of infection,
-              like pain or redness and swelling. See your doctor if you suspect
-              infection.
-            </Text>
+            <SafeAreaView>
+              <ScrollView>
+                <Text
+                  style={[styles.headings, styles.text]}
+                  className="text-white text-lg"
+                >
+                  Begin with washed hands. Gently clean the area with cool to
+                  lukewarm water and mild soap. Remove dirt or other particles
+                  from the wound using sterilized tweezers. For a mild scrape
+                  that’s not bleeding, leave the wound uncovered. If the wound
+                  is bleeding, use a clean cloth or bandage and apply gentle
+                  pressure to the area to stop any bleeding. Cover a wound that
+                  bled with a thin layer of topical antibiotic ointment, like
+                  Bacitracin, or a sterile moisture barrier ointment, like
+                  Aquaphor. Cover it with a clean bandage or gauze. Gently clean
+                  the wound and change the ointment and bandage once per day.
+                  Watch the area for signs of infection, like pain or redness
+                  and swelling. See your doctor if you suspect infection.
+                </Text>
+              </ScrollView>
+            </SafeAreaView>
           </View>
         </View>
       </LinearGradient>
--- screens/components/MedicalInfo.js
+++ screens/components/MedicalInfo.js
@@ -1,5 +1,13 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Pressable,
+} from "react-native";
 import React, { useEffect, useState } from "react";
+import Icon from "react-native-vector-icons/AntDesign";
 import { LinearGradient } from "expo-linear-gradient";
 
 export default function MedicalInfo() {
@@ -10,15 +18,227 @@ export default function MedicalInfo() {
         style={styles.linearGradient}
       >
         <View style={styles.wrapper}>
-          <View style={styles.contentBox}>
-            <Text
-              className="py-6 text-2xl font-bold text-white"
-              style={styles.headings}
-            >
-              Medical Record
-            </Text>
-          </View>
+          <Text
+            className="py-6 text-2xl font-bold text-white"
+            style={styles.headings}
+          >
+            Medical Record
+          </Text>
         </View>
+        <SafeAreaView>
+          <View className="bg-white rounded-lg py-3" style={styles.wrapper}>
+            <Text className="text-xl font-bold mb-2 pl-4" style={styles.color}>
+              Allegies
+            </Text>
+
+            <View style={styles.borderthick} />
+
+            <View className="flex-row py-1 justify-between">
+              <View style={styles.leftrow}>
+                <Text className="pl-4">Allergy</Text>
+              </View>
+              <View style={styles.rightrow}>
+                <Text className="ml-1">Severity</Text>
+              </View>
+              <View className="pr-4">
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#ffffff"
+                  // style={{ marginRight: 6 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthick} />
+
+            <View className="flex-row py-2 justify-between">
+              <View style={styles.leftrow}>
+                <Text className="text-base pl-4" style={styles.color}>
+                  Peanut
+                </Text>
+              </View>
+              <View style={styles.rightrow}>
+                <Text className="text-base" style={styles.color}>
+                  Severe
+                </Text>
+              </View>
+              <View>
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#c2c2c2"
+                  style={{ marginRight: 10 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthin} />
+            <View className="flex-row py-2 justify-between">
+              <View style={styles.leftrow}>
+                <Text className="text-base pl-4" style={styles.color}>
+                  Grass
+                </Text>
+              </View>
+              <View style={styles.rightrow}>
+                <Text className="text-base" style={styles.color}>
+                  Not Severe
+                </Text>
+              </View>
+
+              <View>
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#c2c2c2"
+                  style={{ marginRight: 10 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthin} />
+
+            <View className="flex-row items-center justify-between px-3 py-3">
+              <TextInput
+                className="bg-gray-200 rounded p-2"
+                style={styles.inputsmall}
+                placeholder="Allergy"
+                placeholderTextColor="#a9a9a9"
+              />
+              <TextInput
+                className="bg-gray-200 rounded p-2"
+                style={styles.inputsmall}
+                placeholder="Severity"
+                placeholderTextColor="#a9a9a9"
+              />
+              <Pressable
+                title="Add"
+                className="rounded p-1"
+                style={styles.button}
+              >
+                <Icon name="plus" size={20} color="#fff" />
+              </Pressable>
+            </View>
+            {/**/}
+            <Text
+              className="text-xl font-bold mb-2 mt-5 pl-4"
+              style={styles.color}
+            >
+              Medications
+            </Text>
+            <View style={styles.borderthick} />
+
+            <View className="flex-row py-2 justify-between">
+              <View>
+                <Text className="text-base pl-4" style={styles.color}>
+                  Salbutimol Inhaler
+                </Text>
+              </View>
+
+              <View>
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#c2c2c2"
+                  style={{ marginRight: 10 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthin} />
+            <View className="flex-row py-2 justify-between">
+              <View>
+                <Text className="text-base pl-4" style={styles.color}>
+                  Clonazepam
+                </Text>
+              </View>
+
+              <View>
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#c2c2c2"
+                  style={{ marginRight: 10 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthin} />
+
+            <View className="flex-row items-center justify-between px-3 py-3">
+              <TextInput
+                className="bg-gray-200 rounded p-2"
+                placeholder="Allergy"
+                placeholderTextColor="#a9a9a9"
+                style={{ width: "88%" }}
+              />
+
+              <Pressable
+                title="Add"
+                className="rounded p-1"
+                style={styles.button}
+              >
+                <Icon name="plus" size={20} color="#fff" />
+              </Pressable>
+            </View>
+
+            {/** */}
+            <Text
+              className="text-xl font-bold mb-2 mt-5 pl-4"
+              style={styles.color}
+            >
+              Conditions
+            </Text>
+            <View style={styles.borderthick} />
+
+            <View className="flex-row py-2 justify-between">
+              <View>
+                <Text className="text-base pl-4" style={styles.color}>
+                  Asthma
+                </Text>
+              </View>
+
+              <View>
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#c2c2c2"
+                  style={{ marginRight: 10 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthin} />
+            <View className="flex-row py-2 justify-between">
+              <View>
+                <Text className="text-base pl-4" style={styles.color}>
+                  Anxiety
+                </Text>
+              </View>
+
+              <View>
+                <Icon
+                  name="close"
+                  size={20}
+                  color="#c2c2c2"
+                  style={{ marginRight: 10 }}
+                />
+              </View>
+            </View>
+            <View style={styles.borderthin} />
+
+            <View className="flex-row items-center justify-between px-3 py-3">
+              <TextInput
+                className="bg-gray-200 rounded p-2"
+                placeholder="Condition"
+                placeholderTextColor="#a9a9a9"
+                style={{ width: "88%" }}
+              />
+
+              <Pressable
+                title="Add"
+                className="rounded p-1"
+                style={styles.button}
+              >
+                <Icon name="plus" size={20} color="#fff" />
+              </Pressable>
+            </View>
+          </View>
+        </SafeAreaView>
       </LinearGradient>
     </View>
   );
@@ -29,11 +249,6 @@ const styles = StyleSheet.create({
     width: "90%",
     marginLeft: "auto",
     marginRight: "auto",
-  },
-  contentBox: {
-    marginTop: 90,
-    width: "100%",
-    justifyContent: "center",
   },
   linearGradient: {
     width: "100%",
@@ -46,5 +261,38 @@ const styles = StyleSheet.create({
       height: 4,
     },
     textShadowRadius: 2,
+  },
+  bg: {
+    backgroundColor: "#FE0944",
+  },
+  color: {
+    color: "#555",
+  },
+  borderthick: {
+    height: 2,
+    borderTopWidth: 2,
+    borderColor: "#e6e6e6",
+    borderStyle: "solid",
+    width: "100%",
+  },
+  borderthin: {
+    height: 1,
+    borderTopWidth: 1,
+    borderColor: "#e6e6e6",
+    borderStyle: "solid",
+    width: "100%",
+  },
+  leftrow: {
+    width: 80,
+  },
+  rightrow: { width: 90 },
+  inputsmall: {
+    width: "43%",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 3,
+    backgroundColor: "#FE0944",
   },
 });
