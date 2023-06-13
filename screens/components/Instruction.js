82fc923de8853d1df43c1eacc7fe6c16f7339386
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-13 11:33:13 -0700

    Add dummy content

--- screens/components/Instruction.js
+++ screens/components/Instruction.js
@@ -53,30 +53,27 @@ export default function Instruction(props) {
         colors={["#FE0944", "#FEAE96"]}
         style={styles.linearGradient}
       >
-        <Pressable
-          style={{
-            alignSelf: "flex-end",
-            marginTop: 50,
-            marginRight: 20,
-          }}
-          onPress={saveInstruction}
-        >
-          <Icon
-            name={buttonClick ? "bookmark" : "bookmark-o"}
-            size={30}
-            color="#fff"
-            style={styles.headings}
-          />
-        </Pressable>
-        <View>
+        <View className="pt-20">
           <View style={styles.wrapper}>
+            {/* <Text>{instructionKey}</Text>
+          <Text>{instructionDetail}</Text> */}
             <View className="flex-row py-10 justify-between items-center">
               <Text
                 className="text-4xl text-white font-bold"
                 style={styles.headings}
               >
-                {apiResponse.title}
+                Abrasions
               </Text>
+              <TouchableHighlight onPress={() => {}}>
+                <View>
+                  <Icon
+                    name="bookmark"
+                    size={20}
+                    color="#fff"
+                    style={styles.headings}
+                  />
+                </View>
+              </TouchableHighlight>
             </View>
             <SafeAreaView>
               <ScrollView>
@@ -84,7 +81,18 @@ export default function Instruction(props) {
                   style={[styles.headings, styles.text]}
                   className="text-white text-lg"
                 >
-                  {apiResponse.instruction}
+                  Begin with washed hands. Gently clean the area with cool to
+                  lukewarm water and mild soap. Remove dirt or other particles
+                  from the wound using sterilized tweezers. For a mild scrape
+                  that’s not bleeding, leave the wound uncovered. If the wound
+                  is bleeding, use a clean cloth or bandage and apply gentle
+                  pressure to the area to stop any bleeding. Cover a wound that
+                  bled with a thin layer of topical antibiotic ointment, like
+                  Bacitracin, or a sterile moisture barrier ointment, like
+                  Aquaphor. Cover it with a clean bandage or gauze. Gently clean
+                  the wound and change the ointment and bandage once per day.
+                  Watch the area for signs of infection, like pain or redness
+                  and swelling. See your doctor if you suspect infection.
                 </Text>
               </ScrollView>
             </SafeAreaView>
