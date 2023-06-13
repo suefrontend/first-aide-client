dcab93f22dc3b54ed0d4b5a7854f21181897ef02
lianaa98 <liana.kan98@gmail.com>
2023-06-13 15:29:09 -0700

    allergy routes success

--- screens/components/MedicalRecord/allergy/AllergyList.js
+++ screens/components/MedicalRecord/allergy/AllergyList.js
@@ -11,10 +11,38 @@ import {
 import React, { useEffect, useState } from "react";
 import Icon from "react-native-vector-icons/AntDesign";
 import AllergyItem from "./AllergyItem";
+import { authPost } from "../../../helpers/authenticatedCalls";
 
 export default function AllergyList(props) {
   const { allergies, setAllergies } = props;
-  console.log("allergies", allergies);
+  const [newAllergyName, setNewAllergyName] = useState(null);
+  const [newAllergySeverity, setNewAllergySeverity] = useState(null);
+
+  const addAllergy = async (name, severity) => {
+    try {
+      const response = await authPost("/medicalRecords/allergies", {
+        name: name,
+        severity: severity,
+      });
+      if (response.status === 200) {
+        alert("Allergy added");
+        setAllergies((prev) => [
+          ...prev,
+          {
+            id: response.data.id,
+            name: name,
+            severity: severity,
+          },
+        ]);
+        console.log(allergies);
+      } else {
+        alert("Oops! Something went wrong. Please try again later.");
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
@@ -51,14 +79,31 @@ export default function AllergyList(props) {
           style={styles.inputsmall}
           placeholder="Allergy"
           placeholderTextColor="#a9a9a9"
+          value={newAllergyName}
+          onChangeText={(e) => setNewAllergyName(e)}
         />
         <TextInput
           className="bg-gray-200 rounded p-2"
           style={styles.inputsmall}
           placeholder="Severity"
           placeholderTextColor="#a9a9a9"
+          value={newAllergySeverity}
+          onChangeText={(e) => setNewAllergySeverity(e)}
         />
-        <Pressable title="Add" className="rounded p-1" style={styles.button}>
+        <Pressable
+          title="Add"
+          className="rounded p-1"
+          style={styles.button}
+          onPress={() => {
+            if (!newAllergyName || !newAllergySeverity) {
+              alert("Please fill in all fields");
+              return;
+            }
+            addAllergy(newAllergyName, newAllergySeverity);
+            setNewAllergyName(null);
+            setNewAllergySeverity(null);
+          }}
+        >
           <Icon name="plus" size={20} color="#fff" />
         </Pressable>
       </View>
