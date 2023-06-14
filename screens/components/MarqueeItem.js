1dde7c72d7c9b15d1828a45592279ad71318ae5f
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-14 06:46:36 -0700

    Update style for marquee cards

--- screens/components/MarqueeItem.js
+++ screens/components/MarqueeItem.js
@@ -46,8 +46,8 @@ const styles = StyleSheet.create({
     width: 130,
     height: 77,
     shadowColor: "#000",
-    shadowOffset: { width: 3, height: 3 },
-    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
     elevation: 3,
   },
   text: {
