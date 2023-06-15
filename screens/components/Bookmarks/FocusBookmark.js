8eed356b43b2e6495cc2f76addfb6f1df6d4c710
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-14 21:04:24 -0700

    Fix padding

--- screens/components/Bookmarks/FocusBookmark.js
+++ screens/components/Bookmarks/FocusBookmark.js
@@ -12,7 +12,7 @@ import { LinearGradient } from "expo-linear-gradient";
 import { authPost, authDelete } from "../../helpers/authenticatedCalls";
 import Feather from "react-native-vector-icons/Feather";
 import Icon from "react-native-vector-icons/FontAwesome";
-import AntIcon from "react-native-vector-icons/AntDesign";
+import Entypo from "react-native-vector-icons/Entypo";
 import { FontFamily, ThemeColors } from "../../../theme";
 
 export default function FocusBookmark(props) {
@@ -109,11 +109,11 @@ export default function FocusBookmark(props) {
         <View style={styles.bookmarkInfo}>
           <View style={styles.innerborder}>
             <Pressable onPress={cancelFocusBookmark}>
-              <Icon
-                name="close"
+              <Entypo
+                name="circle-with-cross"
                 size={30}
                 color={ThemeColors.lightgray}
-                style={{ marginLeft: "auto", marginRight: 6, marginTop: 6 }}
+                style={{ marginLeft: "auto", marginRight: 6, marginTop: 7 }}
               />
             </Pressable>
             <ScrollView>
@@ -122,19 +122,7 @@ export default function FocusBookmark(props) {
                   className="text-lg text-center leading-8"
                   style={styles.text}
                 >
-                  {instruction}Contrary to popular belief, Lorem Ipsum is not
-                  simply random text. It has roots in a piece of classical Latin
-                  literature from 45 BC, making it over 2000 years old. Richard
-                  McClintock, a Latin professor at Hampden-Sydney College in
-                  Virginia, looked up one of the more obscure Latin words,
-                  consectetur, from a Lorem Ipsum passage, and going through the
-                  cites of the word in classical literature, discovered the
-                  undoubtable source. Lorem Ipsum comes from sections 1.10.32
-                  and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
-                  of Good and Evil) by Cicero, written in 45 BC. This book is a
-                  treatise on the theory of ethics, very popular during the
-                  Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor
-                  sit amet..", comes from a line in section 1.10.32.
+                  {instruction}
                 </Text>
               </View>
             </ScrollView>
