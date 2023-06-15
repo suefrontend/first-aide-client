f8d9095d1dd89dc9ae2b539aec8d2f497591a327
lianaa98 <liana.kan98@gmail.com>
2023-06-15 13:02:53 -0700

    implement drawer

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,6 +11,7 @@ import { useFonts } from "expo-font";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import Icon from "react-native-vector-icons/FontAwesome";
 import Entypo from "react-native-vector-icons/Entypo";
 import Ionicons from "@expo/vector-icons/Ionicons";
@@ -24,6 +25,7 @@ import EmergencyContact from "./components/EmergContacts/EmergencyContact";
 import Instruction from "./components/Instruction";
 
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function HomeScreen(props) {
   const { logoutHandler } = props;
@@ -46,6 +48,14 @@ export default function HomeScreen(props) {
     return null;
   }
 
+  const DrawerContent = ({ navigation }) => {
+    return (
+      <View>
+        <Text>Drawer Content</Text>
+      </View>
+    );
+  };
+
   function HomeTabs() {
     const navigation = useNavigation();
     const tabOffsetValue = useRef(new Animated.Value(0)).current;
@@ -60,184 +70,215 @@ export default function HomeScreen(props) {
 
     return (
       <>
-        <Tab.Navigator
-          initialRouteName="Recorder"
+        <Drawer.Navigator
+          drawerContent={(props) => <DrawerContent {...props} />}
           screenOptions={{
-            tabBarShowLabel: false,
-            tabBarStyle: {
-              // backgroundColor: "white",
-              // position: "absolute",
-              // bottom: 25,
-              // marginHorizontal: 20,
-              // paddingHorizontal: 20,
-              height: 75,
-              // borderRadius: 8,
-              shadowColor: "#000",
-              shadowOpacity: 0.1,
-              shadowOffset: {
-                width: 4,
-                height: -4,
-              },
+            headerShown: false,
+            drawerStyle: {
+              backgroundColor: "transparent",
+              width: "50%",
             },
+            drawerActiveBackgroundColor: "transparent",
+            drawerInactiveBackgroundColor: "transparent",
+            drawerActiveTintColor: "transparent",
+            drawerInactiveTintColor: "transparent",
           }}
         >
-          <Tab.Screen
-            name="Bookmark"
-            component={Bookmark}
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused }) => (
-                <View style={{ position: "absolute", top: "30%" }}>
-                  <Entypo
-                    name="bookmark"
-                    size={26}
-                    color={focused ? ThemeColors.red : ThemeColors.navicon}
-                  />
-                </View>
-              ),
-            }}
-            listeners={({ navigation, route }) => ({
-              tabPress: (e) => {
-                Animated.spring(tabOffsetValue, {
-                  toValue: 0,
-                  useNativeDriver: true,
-                }).start();
-              },
-            })}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="Medical Info"
-            component={MedicalRecordScreen}
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused }) => (
-                <View style={{ position: "absolute", top: "30%" }}>
-                  <Icon
-                    name="stethoscope"
-                    size={26}
-                    color={focused ? ThemeColors.red : ThemeColors.navicon}
-                  />
-                </View>
-              ),
-            }}
-            listeners={({ navigation, route }) => ({
-              tabPress: (e) => {
-                Animated.spring(tabOffsetValue, {
-                  toValue: getWidth() + 6,
-                  useNativeDriver: true,
-                }).start();
-              },
-            })}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="Recorder"
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused }) => (
-                <View
-                  style={{
-                    position: "absolute",
-                    top: "10%",
-                    width: 50,
-                    height: 50,
-                    // backgroundColor: ThemeColors.navicon,
-                    borderRadius: 25,
-                    justifyContent: "center",
-                    alignContent: "center",
-                    // marginBottom: 30,
-                  }}
-                >
-                  <Icon
-                    name="microphone"
-                    size={36}
-                    color={focused ? ThemeColors.red : ThemeColors.navicon}
-                    style={{ marginLeft: "auto", marginRight: "auto" }}
-                  />
-                </View>
-              ),
-            }}
-            listeners={({ navigation, route }) => ({
-              tabPress: (e) => {
-                Animated.spring(tabOffsetValue, {
-                  toValue: getWidth() * 2 + 18,
-                  useNativeDriver: true,
-                }).start();
-              },
-            })}
-          >
+          <Drawer.Screen name="Main">
             {() => (
-              <Recorder
-                logoutHandler={logoutHandler}
-                navigation={navigation}
-                setApiResponse={setApiResponse}
-              />
+              <Tab.Navigator
+                initialRouteName="Recorder"
+                screenOptions={{
+                  tabBarShowLabel: false,
+                  tabBarStyle: {
+                    // backgroundColor: "white",
+                    // position: "absolute",
+                    // bottom: 25,
+                    // marginHorizontal: 20,
+                    // paddingHorizontal: 20,
+                    height: 75,
+                    // borderRadius: 8,
+                    shadowColor: "#000",
+                    shadowOpacity: 0.1,
+                    shadowOffset: {
+                      width: 4,
+                      height: -4,
+                    },
+                  },
+                }}
+              >
+                <Tab.Screen
+                  name="Bookmark"
+                  component={Bookmark}
+                  options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                      <View style={{ position: "absolute", top: "30%" }}>
+                        <Entypo
+                          name="bookmark"
+                          size={26}
+                          color={
+                            focused ? ThemeColors.red : ThemeColors.navicon
+                          }
+                        />
+                      </View>
+                    ),
+                  }}
+                  listeners={({ navigation, route }) => ({
+                    tabPress: (e) => {
+                      Animated.spring(tabOffsetValue, {
+                        toValue: 0,
+                        useNativeDriver: true,
+                      }).start();
+                    },
+                  })}
+                ></Tab.Screen>
+                <Tab.Screen
+                  name="Medical Info"
+                  component={MedicalRecordScreen}
+                  options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                      <View style={{ position: "absolute", top: "30%" }}>
+                        <Icon
+                          name="stethoscope"
+                          size={26}
+                          color={
+                            focused ? ThemeColors.red : ThemeColors.navicon
+                          }
+                        />
+                      </View>
+                    ),
+                  }}
+                  listeners={({ navigation, route }) => ({
+                    tabPress: (e) => {
+                      Animated.spring(tabOffsetValue, {
+                        toValue: getWidth() + 6,
+                        useNativeDriver: true,
+                      }).start();
+                    },
+                  })}
+                ></Tab.Screen>
+                <Tab.Screen
+                  name="Recorder"
+                  options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                      <View
+                        style={{
+                          position: "absolute",
+                          top: "10%",
+                          width: 50,
+                          height: 50,
+                          // backgroundColor: ThemeColors.navicon,
+                          borderRadius: 25,
+                          justifyContent: "center",
+                          alignContent: "center",
+                          // marginBottom: 30,
+                        }}
+                      >
+                        <Icon
+                          name="microphone"
+                          size={36}
+                          color={
+                            focused ? ThemeColors.red : ThemeColors.navicon
+                          }
+                          style={{ marginLeft: "auto", marginRight: "auto" }}
+                        />
+                      </View>
+                    ),
+                  }}
+                  listeners={({ navigation, route }) => ({
+                    tabPress: (e) => {
+                      Animated.spring(tabOffsetValue, {
+                        toValue: getWidth() * 2 + 18,
+                        useNativeDriver: true,
+                      }).start();
+                    },
+                  })}
+                >
+                  {() => (
+                    <Recorder
+                      logoutHandler={logoutHandler}
+                      navigation={navigation}
+                      setApiResponse={setApiResponse}
+                    />
+                  )}
+                </Tab.Screen>
+                <Tab.Screen
+                  name="Hospital Info"
+                  component={HospitalInfo}
+                  options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                      <View style={{ position: "absolute", top: "30%" }}>
+                        <Icon
+                          name="medkit"
+                          size={26}
+                          color={
+                            focused ? ThemeColors.red : ThemeColors.navicon
+                          }
+                        />
+                      </View>
+                    ),
+                  }}
+                  listeners={({ navigation, route }) => ({
+                    tabPress: (e) => {
+                      Animated.spring(tabOffsetValue, {
+                        toValue: getWidth() * 3 + 26,
+                        useNativeDriver: true,
+                      }).start();
+                    },
+                  })}
+                ></Tab.Screen>
+                <Tab.Screen
+                  name="Emergency Contacts"
+                  component={EmergencyContact}
+                  options={{
+                    headerShown: false,
+                    tabBarIcon: ({ focused }) => (
+                      <View style={{ position: "absolute", top: "30%" }}>
+                        <Icon
+                          name="phone"
+                          size={26}
+                          color={
+                            focused ? ThemeColors.red : ThemeColors.navicon
+                          }
+                        />
+                      </View>
+                    ),
+                  }}
+                  listeners={({ navigation, route }) => ({
+                    tabPress: (e) => {
+                      Animated.spring(tabOffsetValue, {
+                        toValue: getWidth() * 4 + 36,
+                        useNativeDriver: true,
+                      }).start();
+                    },
+                  })}
+                ></Tab.Screen>
+              </Tab.Navigator>
             )}
-          </Tab.Screen>
-          <Tab.Screen
-            name="Hospital Info"
-            component={HospitalInfo}
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused }) => (
-                <View style={{ position: "absolute", top: "30%" }}>
-                  <Icon
-                    name="medkit"
-                    size={26}
-                    color={focused ? ThemeColors.red : ThemeColors.navicon}
-                  />
-                </View>
-              ),
-            }}
-            listeners={({ navigation, route }) => ({
-              tabPress: (e) => {
-                Animated.spring(tabOffsetValue, {
-                  toValue: getWidth() * 3 + 26,
-                  useNativeDriver: true,
-                }).start();
-              },
-            })}
-          ></Tab.Screen>
-          <Tab.Screen
-            name="Emergency Contacts"
-            component={EmergencyContact}
-            options={{
-              headerShown: false,
-              tabBarIcon: ({ focused }) => (
-                <View style={{ position: "absolute", top: "30%" }}>
-                  <Icon
-                    name="phone"
-                    size={26}
-                    color={focused ? ThemeColors.red : ThemeColors.navicon}
-                  />
-                </View>
-              ),
-            }}
-            listeners={({ navigation, route }) => ({
-              tabPress: (e) => {
-                Animated.spring(tabOffsetValue, {
-                  toValue: getWidth() * 4 + 36,
-                  useNativeDriver: true,
-                }).start();
-              },
-            })}
-          ></Tab.Screen>
-        </Tab.Navigator>
-        {/* <Animated.View
-          style={{
-            width: getWidth() - 20,
-            height: 2,
-            backgroundColor: ThemeColors.red,
-            position: "absolute",
-            bottom: 80,
-            left: 17,
-            borderRadius: 50,
-            transform: [{ translateX: tabOffsetValue }],
-          }}
-        ></Animated.View> */}
+          </Drawer.Screen>
+        </Drawer.Navigator>
       </>
     );
   }
 
+  {
+    /* <Animated.View
+    style={{
+      width: getWidth() - 20,
+      height: 2,
+      backgroundColor: ThemeColors.red,
+      position: "absolute",
+      bottom: 80,
+      left: 17,
+      borderRadius: 50,
+      transform: [{ translateX: tabOffsetValue }],
+    }}
+  ></Animated.View> */
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator>
---
+++ screens/components/Logout.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+
+export default function Logout() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Change name and Log out screen?</Text>
+    </View>
+  );
+}
