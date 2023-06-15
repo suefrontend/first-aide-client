2b8ba67f127009b6352e76282f27a5454db9178b
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-14 19:40:22 -0700

    Fix scrolling issue in Medical record page

--- screens/components/MedicalRecord/MedicalRecordScreen.js
+++ screens/components/MedicalRecord/MedicalRecordScreen.js
@@ -52,7 +52,7 @@ export default function MedicalRecordScreen() {
             >
               Medical Record
             </Text>
-            <View>
+            <View style={{ paddingBottom: 110 }}>
               <ScrollView>
                 <View
                   className="bg-white rounded-lg py-3 mb-2 w-full"
