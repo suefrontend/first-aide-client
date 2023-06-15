4a3bb7abf0cbb5fa0c3e379423d88dc013f5559d
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-15 09:23:50 -0700

    Animation effect - half way

--- screens/components/Marquee/Marquee.js
+++ screens/components/Marquee/Marquee.js
@@ -1,11 +1,29 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import React, { useRef, useEffect } from "react";
+import { View, Text, FlatList, StyleSheet, Animated } from "react-native";
 import MarqueeItem from "./MarqueeItem";
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 export default function Marquee(props) {
   const { marqueeItems, clickMarqueeHandler } = props;
+  // const ref = useRef(null);
+
+  const offset = new Animated.Value(0);
+  const carouselRef = React.useRef();
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const newIndex = Math.floor(offset._value) + 1;
+      carouselRef.current.scrollToIndex({ index: newIndex, animated: true });
+      console.log("new index", newIndex);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
-    <FlatList
+    <AnimatedFlatList
+      ref={carouselRef}
       horizontal={true}
       data={marqueeItems}
       keyExtractor={(marqueeItem) => marqueeItem.id}
